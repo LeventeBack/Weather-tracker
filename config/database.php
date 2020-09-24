2a042da029ef8cc5725e14d85f28687b156d9877
LeventeBack <backistvanlevente17gmail.com>
2020-09-24 20:10:52 +0300

    Time fix 2

--- config/database.php
+++ config/database.php
@@ -40,7 +40,6 @@ return [
             'url' => env('DATABASE_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
-            'timezone'  => '-03:00',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
@@ -59,7 +58,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'timezone'  => '-03:00',
+            //'timezone'  => '-03:00',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
@@ -78,7 +77,6 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
-            'timezone'  => '-03:00',
         ],
 
         'sqlsrv' => [
@@ -92,7 +90,6 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'timezone'  => '-03:00',
         ],
 
     ],
